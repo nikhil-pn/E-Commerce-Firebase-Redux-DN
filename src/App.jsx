6f8d29d6e811cac93c21f72fab926f7f327cdc6d
nikhilpn725 <nikhilpn725@gmail.com>
2023-03-02 13:26:14 +0530

    React Routing created and implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import Button from "@mui/material/Button";
 
+import { createBrowserRouter } from "react-router-dom";
+import { createRoutesFromElements } from "react-router-dom";
+import { Route } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<h1>THis is index page</h1>} />
+  )
+);
+
 function App() {
-  return (
-    <>
-      <h1> This is APP</h1>
-      <Button variant="contained">Button Click me</Button>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
