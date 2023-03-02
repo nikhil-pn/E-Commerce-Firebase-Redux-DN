8d1d48182087059471b79d3886bee146de84b6cc
nikhilpn725 <nikhilpn725@gmail.com>
2023-03-02 15:50:37 +0530

    home page fetch all Product api added in useEffect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export default function Home() {
+  const [products, setproducts] = useState(["prodcut"]);
+
+  useEffect(() => {
+    fetchAllProducts();
+  }, []);
+
+  async function fetchAllProducts() {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const result = await response.json();
+    setproducts(result);
+  }
   return (
-    <div>Home</div>
-  )
+    <div>
+      <pre>{JSON.stringify(products, null, 2)}</pre>
+    </div>
+  );
 }
