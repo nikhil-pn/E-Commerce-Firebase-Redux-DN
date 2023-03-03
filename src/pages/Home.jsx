294f9c69aa02b89edb577cfa0f12b43c7714fbed
nikhilpn725 <nikhilpn725@gmail.com>
2023-03-03 18:33:36 +0530

    fetchAllProduct() has been moved to reducer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,23 +14,19 @@ import {
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addToCart } from "../feature/cart-slice";
+import { fetchAllProducts } from "../feature/products-slice";
 
 export default function Home() {
   const theme = useTheme();
-  const [products, setproducts] = useState(["prodcut"]);
+  const state = useSelector((state) => state.products);
+
+  const { value: products, loading } = state ?? {};
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    fetchAllProducts();
-  }, []);
-
-  async function fetchAllProducts() {
-    const response = await fetch("https://fakestoreapi.com/products");
-    const result = await response.json();
-    console.log("result", result);
-    setproducts(result);
+  if (!products?.length) {
+    dispatch(fetchAllProducts());
   }
 
   function addProductToCart(product) {
@@ -40,7 +36,7 @@ export default function Home() {
   return (
     <Container sx={{ py: 8 }} maxWidth="8">
       <Grid container spacing={4}>
-        {products.map(({ title, id, price, description, rating, image }) => (
+        {products?.map(({ title, id, price, description, rating, image }) => (
           <Grid item key={id} xs={12} sm={6} md={3}>
             <Card
               sx={{ height: "100%", display: "flex", flexDirection: "column" }}
